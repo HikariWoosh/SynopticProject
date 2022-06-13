7b68694a0f81f69c545c6fa21af848aad7337b04
aillsley <107123206+aillsley@users.noreply.github.com>
2022-06-13 12:37:35 +0100

    Update home.js

--- home.js
+++ home.js
@@ -19,16 +19,23 @@ function openTab(evt, userType) {
     document.getElementById(userType).style.display = "block";
     evt.currentTarget.className += " active";
   }
-
   document.getElementById("defaultOpen").click();
 
-  //function getNumber is used to store the phone number that the user will input
+   //function getNumber is used to store the phone number that the user will input
 function getNumber(x){
-    var x;
-    
-    x = document.getElementById("phoneNumber").value;
-    document.getElementById("phoneNumber").value = '';
-    //testing to see if it has stored the value
-    console.log(x)
+  var x;
+  
+  x = document.getElementById("phoneNumber").value;
 
-  }
+  document.getElementById("phoneNumber").value = '';
+  //testing to see if it has stored the value
+  console.log(x)
+
+}
+
+//Restricts input into text field to only numbers
+
+function restrictInput(textField){
+  var number = 
+  textField.addEventListener(event, )
+}
